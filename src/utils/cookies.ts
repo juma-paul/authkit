a70b6050a4c5f27de61545abc5212c9a95eae5c8
juma-paul <raisben7@gmail.com>
2026-04-14 23:19:20 -0700

    fix: set sameSite to non for prod and lax for local dev

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -3,7 +3,10 @@ import { Response } from "express";
 const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "lax" as const,
+  sameSite:
+    process.env.NODE_ENV === "production"
+      ? ("none" as const)
+      : ("lax" as const),
 };
 
 export const setTokenCookies = (
